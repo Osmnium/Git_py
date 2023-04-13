a5858c7f84ae3dc3682fe245f2ac33ef6c02d905
Osman <osmansalimov007@gmail.com>
2023-04-13 17:15:56 +0400

    Последняя версия

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-def aboba(a, b):
-    return a + b
+class worker:
+    def __init__(self, wage, weekend, my_lunch):
+        self.wage = wage
+        self.weekend = weekend
+        self.my_lunch = my_lunch
 
-def mult(a, b):
-    return a * b
+    def get_wage(self):
+        return f'I get a wage in the size of {self.wage} dollars!'
+
+    def brain_recharge(self):
+        return f'I will rest for {self.weekend} days!'
+
+programmer = worker(90000, 2, 'banana')
+print(programmer.get_wage())
+
+class Builder(worker):
+
+    def __init__(self, wage, weekend, my_lunch, __special_form):
+        super().__init__(wage, weekend, my_lunch)
+        self.__special_form = "builder's form"
+    def brain_recharge(self):
+        return f'I will rest for {self.weekend} months!'
